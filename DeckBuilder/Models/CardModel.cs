dff51a302027530fbab0f5e3c75e8c45d259725b
MasonSling <mason@slinger.land>
2024-11-17 19:06:23 -0500

    Inital commit

---
+++ DeckBuilder/Models/CardModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DeckBuilder.Models
+{
+    public class CardModel
+    {
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public string Type { get; set; }
+        public string Text { get; set; }
+
+        public int PlayerId { get; set; }
+    }
+}
---
+++ DeckBuilder/Models/PlayerModel.cs
@@ -0,0 +1,16 @@
+﻿namespace DeckBuilder.Models
+{
+    public class PlayerModel
+    {
+        public string Name { get; set; }
+        public List<CardModel> Deck { get; set; } = new();
+        public List<CardModel> DiscardPile { get; set; } = new();
+        public List<CardModel> Hand { get; set; } = new();
+        public List<CardModel> PlayedCards { get; set; } = new();
+
+        public PlayerModel(string name)
+        {
+            Name = name;
+        }
+    }
+}
